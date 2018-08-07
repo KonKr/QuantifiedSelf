3205a86a8cffa9aefc77a060ea45d2e93fb95ef7
Konstantinos Kritikakis <kon.kri@outlook.com>
2018-08-07 17:36:42 +0300

    code refactoring, token generation

--- WebApi/Controllers/AuthController.cs
+++ WebApi/Controllers/AuthController.cs
@@ -8,29 +8,22 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
+using WebApi.Services;
+using WebApi.ViewModels;
 
 namespace WebApi.Controllers
 {
     [Produces("application/json")]
-    [Route("api/Auth")]
     public class AuthController : Controller
     {
-        [Route("Token")]
-        [HttpGet]
-        public IActionResult Token()
+        //Get Access Token in order to use other methods...
+        [Route("api/Auth/Token")]
+        [HttpPost]
+        public IActionResult GetAuthorizationToken([FromBody] GetToken_Credentials credentials)
         {
-            var claimsData = new[] { new Claim(ClaimTypes.Name, "kapoios"), new Claim(ClaimTypes.PrimarySid, "adsfasdffwfw") };
-            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("helloworldasdfasdfasdfasdfasdfasdfasdfasdfasdfasdf"));
-            var signInCred = new SigningCredentials(key, SecurityAlgorithms.HmacSha256Signature);
-            var token = new JwtSecurityToken(
-                    issuer: "Moi",
-                    audience: "Moi",
-                    expires: DateTime.Now.AddMinutes(1),
-                    claims: claimsData,
-                    signingCredentials: signInCred
-                );
-            var tokenString = new JwtSecurityTokenHandler().WriteToken(token);
-            return Ok(tokenString);
+
+            var token = JWT_Authentication.GetToken(credentials.Email);
+            return Ok(token);
         }
     }
 }
---
+++ WebApi/Services/JWT_Authentication.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApi.Services
+{
+    public static class JWT_Authentication
+    {
+        private static Claim[] ClaimsData { get; set; }
+        private static SymmetricSecurityKey Key { get; set; }
+        private static SigningCredentials SigningCredentials { get; set; }
+
+
+        public static String GetToken(String name)
+        {
+            //First produce the JWT Token Object...
+            var tokenObj = ProduceTokenObject(name);
+            var tokenStr = ProduceTokenString(tokenObj);
+            return tokenStr;
+        }
+
+        private static JwtSecurityToken ProduceTokenObject(String name)
+        {
+            //Define Values...
+            ClaimsData = new[] {
+                new Claim(ClaimTypes.Name, name)
+            };
+            Key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("prepei_na_vroume_ena_kleidi_na_valoume_edw"));
+            SigningCredentials = new SigningCredentials(Key, SecurityAlgorithms.HmacSha256Signature);
+
+            var JWT_Token_Object = new JwtSecurityToken(
+                issuer: "Moi",
+                audience: "Toi",
+                expires: DateTime.Now.AddMinutes(1),
+                claims: ClaimsData,
+                signingCredentials: SigningCredentials
+            );
+            return JWT_Token_Object;
+        }
+
+        private static String ProduceTokenString(JwtSecurityToken tokenObject)
+        {
+            //Produce token...
+            var tokenString = new JwtSecurityTokenHandler().WriteToken(tokenObject);
+            return tokenString;
+        }
+    }
+}
